a7f5421ab6c656a50f1ba66205098ac83c79b2fe
Ncia89 <nicholas.ciarelli@gmail.com>
2019-12-12 17:24:23 -0500

    Made start on implementing database functionality.

--- init-db.sql
+++ init-db.sql
@@ -4,3 +4,8 @@ CREATE TABLE shopping_cart(
 	price real,
 	quantity SMALLINT
 );
+INSERT INTO shopping_cart (product, price, quantity)
+VALUES ('bananas', 0.49, 3),
+('celery', 1.33, 2),
+('apples', 1.39, 3),
+('bread', 1.25, 2);
